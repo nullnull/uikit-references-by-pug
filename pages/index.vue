fd35fa6cf97d760ecf8b887af15d059e78b79082
Katsuma Narisawa <face.the.music721@gmail.com>
2019-04-28 20:06:41 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  main
+    | hoge
+</template>
+
+<script>
+export default {
+  components: {},
+  mounted() {},
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ plugins/uikit.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+import 'uikit/dist/css/uikit.css'
+
+// loads the Icon plugin
+UIkit.use(Icons)
+Vue.prototype.$uikit = UIkit
