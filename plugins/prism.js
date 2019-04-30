b14d8b966da006806ab2bf98e2de16c8602c0f58
Katsuma Narisawa <face.the.music721@gmail.com>
2019-04-30 22:40:26 +0900

    Partialize

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,5 +1,4 @@
 import Prism from 'prismjs'
 import PrismPug from 'prismjs/components/prism-pug'
 import 'prismjs/themes/prism-tomorrow.css'
-// import loadLanguages from 'prismjs/components/'
 export default Prism
